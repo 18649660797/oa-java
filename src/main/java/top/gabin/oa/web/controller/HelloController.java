f5ea4529e8ce7ffdf4d8852f756a23f1c0a0e352
linjb <linjb@ecmaster.cn>
2015-12-10 13:33:58 +0800

    1、配置数据库版本控制 2、配置日志打印

--- src/main/java/top/gabin/oa/web/controller/HelloController.java
+++ src/main/java/top/gabin/oa/web/controller/HelloController.java
@@ -1,5 +1,7 @@
 package top.gabin.oa.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+		logger.info("hello");
 		return "hello";
 	}
 }
