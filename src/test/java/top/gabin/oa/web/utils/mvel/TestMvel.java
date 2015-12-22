fc814f2d9f6ad0fce1f875f9dad269b385bd2f13
linjb <linjb@ecmaster.cn>
2015-12-23 07:30:39 +0800

    去除测试类，还无法正常启动系统

---
+++ src/test/java/top/gabin/oa/web/utils/mvel/TestMvel.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015 云智盛世
+ * Created with TestMvel.
+ */
+package top.gabin.oa.web.utils.mvel;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+
+/**
+ * @author linjiabin  on  15/12/22
+ */
+public class TestMvel {
+
+    @Test
+    public void testExecute() {
+        Map<String, Object> vars = new HashMap<String, Object>();
+        vars.put("a", 2);
+        vars.put("b", 1);
+        assert MvelUtils.eval("a > b", vars);
+        System.out.println("测试成功");
+    }
+
+}
