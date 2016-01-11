a1df001cce6a17532a039b97aed2f089e5340de2
linjb <linjb@ecmaster.cn>
2016-01-11 16:00:32 +0800

    测试环境数据库搭建

--- src/test/java/top/gabin/oa/BaseTest.java
+++ src/test/java/top/gabin/oa/BaseTest.java
@@ -4,11 +4,20 @@
  */
 package top.gabin.oa;
 
+import org.flywaydb.core.Flyway;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author linjiabin  on  15/12/23
@@ -31,5 +40,28 @@ import org.springframework.transaction.annotation.Transactional;
 })
 @Transactional
 public class BaseTest {
+    @Resource(name = "dataSource")
+    private DataSource dataSource;
+    @Before
+    public void before() {
+        try {
+            Flyway flyway = new Flyway();
+            flyway.setBaselineOnMigrate(true);//指定生成一条初始版本的记录
+            flyway.setBaselineVersion("2015.01.01.01.01.00");
+            flyway.setBaselineDescription("初始版本");
+            flyway.setValidateOnMigrate(true);
+            flyway.setOutOfOrder(true);//指定如果有版本比现在早的也同时执行
+            flyway.setEncoding("utf-8");
+            flyway.setDataSource(dataSource);
+            flyway.migrate();
+        } catch (Exception e) {
+            throw new RuntimeException("执行数据库更新时出错。请处理，不然启动不了",e);
+        }
+    }
+
+    @After
+    public void after() throws SQLException {
+
+    }
 
 }
--- src/test/java/top/gabin/oa/web/service/PermissionServiceTests.java
+++ src/test/java/top/gabin/oa/web/service/PermissionServiceTests.java
@@ -4,6 +4,7 @@
  */
 package top.gabin.oa.web.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
