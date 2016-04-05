581adb6ecc7a20af6150de26996c7d1e06b98935
linjb <linjb@ecmaster.cn>
2016-04-05 15:55:54 +0800

    bit test

---
+++ src/test/java/top/gabin/oa/think/BitOperaTest.java
@@ -0,0 +1,16 @@
+package top.gabin.oa.think;
+
+import org.junit.Test;
+
+/**
+ * Created by linjiabin on 16/4/5.
+ */
+public class BitOperaTest {
+
+    @Test
+    public void testAnd() {
+        System.out.println(Integer.toBinaryString(21));
+        System.out.println(Integer.toBinaryString(21 & 0xff));
+    }
+
+}
