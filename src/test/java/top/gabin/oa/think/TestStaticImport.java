4d3e63dc8deded64a22cd87b9697be099cff7b6a
linjb <linjb@ecmaster.cn>
2016-04-05 14:44:03 +0800

    static import demo

---
+++ src/test/java/top/gabin/oa/think/TestStaticImport.java
@@ -0,0 +1,11 @@
+package top.gabin.oa.think;
+
+import static top.gabin.oa.think.Print.*;
+/**
+ * Created by linjiabin on 16/4/5.
+ */
+public class TestStaticImport {
+    public static void main(String[] args) {
+        print("hello");
+    }
+}
