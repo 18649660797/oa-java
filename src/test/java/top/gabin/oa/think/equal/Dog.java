c840adce488e91e4cfdca927f882a5c2aeac857e
linjb <linjb@ecmaster.cn>
2016-04-05 14:50:58 +0800

    equal demo

---
+++ src/test/java/top/gabin/oa/think/equal/Dog.java
@@ -0,0 +1,33 @@
+package top.gabin.oa.think.equal;
+
+/**
+ * Created by linjiabin on 16/4/5.
+ */
+public class Dog {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
